a9ce093409bd712c5c40918d1034d328fda8d4df
Caio Bahlis <cbahlis@usf.edu>
2024-08-11 22:47:34 -0300

    Starting Game

--- Truco_Server.go
+++ Truco_Server.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"Truco_Server/Truco_Server/cardpack"
 	"Truco_Server/cardpack"
+	"bytes"
 	"fmt"
 	"math/rand"
 	"net"
 	"time"
 )
 
+type ServerStruct struct{
+	Port string
+	Clients []Client
+}
+
 type Client struct{
-	Name string
-	IpAddress string
+	IpAddress net.Conn
 }
 
 
 func main(){
-	Server, err := net.Listen("tcp", ":8080")
+	MyServer := ServerStruct{Port: ":8080", Clients: []Client{}}
+	Server, err := net.Listen("tcp", MyServer.Port)
 	if err != nil{
 		fmt.Println("Error Openning the server")
 		return
@@ -27,17 +34,29 @@ func main(){
 			fmt.Println("Error accepting Client")
 			return
 		}
+		MyServer.Clients = append(MyServer.Clients, Client{connection})
 		go ListenToMe(connection)
 		
 		fmt.Println(connection)
 		message := []byte("Hello, World")
 		connection.Write(message)
+		/*
+		if len(MyServer.Clients) {
+			Waiting_Message, _:= "Waiting For Players... %d/4", len(MyServer.Clients)
+			connection.Write([]byte(Waiting_Message))
+		}else{
+			connection.Write([]byte("Starting Match...."))
+			
+		}
+		*/
+		connection.Write(byte("Starting Match..."))
+		MyServer.Start_Game()
 	}
 
 }
 
 
-func ShuffleHands(){
+func ShuffleHands() []cardpack.Card{
 	Hands := []cardpack.Card{}
 	
 	rand.Seed(time.Now().UnixNano())
@@ -47,7 +66,7 @@ func ShuffleHands(){
 		Hands = append(Hands, cardpack.Card{Name: cardpack.Cards[i], Value: cardpack.Values[cardpack.Cards[i]]})
 	}
 
-	fmt.Println(Hands)
+	return Hands
 }
 
 func ListenToMe(connection net.Conn){
@@ -56,4 +75,9 @@ func ListenToMe(connection net.Conn){
 		connection.Read(mybuff)
 		fmt.Println(string(mybuff[:]))
 	}
+}
+
+func (S *ServerStruct) Start_Game(){
+	Card := ShuffleHands()
+	S.Clients[0].IpAddress.Write([]byte(Card[0].Name))
 }
